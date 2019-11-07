8cbb5db80ca04b8cb3a4b8a96aed90f430383754
Alexandre <alexandre.dionizio@sp.senac.br>
2019-11-06 21:54:51 -0300

    primeiro commit

---
+++ Conceitos_OO/Item.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Conceitos_OO
+{
+    class Item
+    {
+        public string NomeItem;
+        public double ValorItem;
+        public int QuantidadeItem;
+
+        public double AtualizaInventario()
+        {
+            return ValorItem * QuantidadeItem;
+        }
+
+        public void AdicionaItens(int quantidade)
+        {
+            QuantidadeItem += quantidade;
+        }
+
+        public void RemoveItens(int quantidade)
+        {
+            QuantidadeItem -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return NomeItem
+                + ", $ "
+                + ValorItem.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + QuantidadeItem
+                + " unidades, Total: $"
+                + AtualizaInventario().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- Conceitos_OO/Program.cs
+++ Conceitos_OO/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Conceitos_OO
 {
@@ -10,6 +12,34 @@ namespace Conceitos_OO
     {
         static void Main(string[] args)
         {
+            //Intanciar a classe Item
+            Item i = new Item();
+            WriteLine("Entre com os dados do item: ");
+            Write("Nome do item: ");
+            i.NomeItem = ReadLine();
+            Write("Valor do item: ");
+            i.ValorItem = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+            Write("Quantidade no inventário: ");
+            i.QuantidadeItem = int.Parse(ReadLine());
+
+            WriteLine();
+            Write("Dados do item: " + i);
+
+            WriteLine("");
+            Write("Digite o número de itens a ser adicionado ao inventário: ");
+            int qtdA = int.Parse(ReadLine());
+            i.AdicionaItens(qtdA);
+
+            WriteLine("");
+            WriteLine("Dados atualizados: " + i);
+
+            WriteLine("");
+            Write("Digite o número de itens a ser removido do inventário: ");
+            int qtdR = int.Parse(ReadLine());
+            i.RemoveItens(qtdR);
+
+            WriteLine("");
+            WriteLine("Dados atualizados: " + i);
         }
     }
 }
